e5f95046ffe2f0589725a63fbe62e0d2a12ef47d
Rafael Garcia <rafagrcb@gmail.com>
2020-10-11 04:13:03 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    appointments.map do |appointments|
+      appointments.patient
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+end
